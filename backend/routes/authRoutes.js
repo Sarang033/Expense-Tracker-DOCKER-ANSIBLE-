a909073e0a6677c487beb2a1cc7f0511dd5c96d1
SARANG SHARMA <sarangchamp2004@gmail.com>
2024-10-21 14:51:57 +0530

    error

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 const { registerUser, loginUser } = require('../controllers/authController');
 
-router.post('/register', (req, res, next) => {
+router.post('/api/register', (req, res, next) => {
   console.log('Register route hit');
   registerUser(req, res, next);
 });
 
-router.post('/login', (req, res, next) => {
+router.post('/api/login', (req, res, next) => {
   console.log('Login route hit');
   loginUser(req, res, next);
 });
